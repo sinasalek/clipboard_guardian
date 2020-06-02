c040120284a1c7dc2adfb022b0418a67151cecf0
sinasalek <sinasalek@gmail.com>
2020-06-02 18:20:48 +0430

    Config parameters updated

--- src/config.rs
+++ src/config.rs
@@ -4,18 +4,22 @@ use serde::Deserialize;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ConfyConfig {
-    pub interval: u64
+    pub interval: u64,
+    pub history_total: u64,
+    pub restore_hot_key: String,
 }
 
 impl Default for ConfyConfig {
     fn default() -> Self {
         ConfyConfig {
-            interval: 15
+            interval: 15,
+            history_total: 5,
+            restore_hot_key: String::from("ctrl+shift+J")
         }
     }
 }
 
 pub fn config_get() -> Result<ConfyConfig, confy::ConfyError> {
-    let cfg: ConfyConfig = confy::load("clipboard_vanisher")?;
+    let cfg: ConfyConfig = confy::load("clipboard_guardian")?;
     Ok(cfg)
 }
